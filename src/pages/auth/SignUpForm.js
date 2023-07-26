3c4b32399510173a860b7c4308a76271c9271afa
KarlTred265 Tred <tred26559@gmail.com>
2023-07-26 18:19:12 +0000

    add Redirecting the User

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -15,8 +15,10 @@ import {
   Alert,
 } from "react-bootstrap";
 import axios from "axios";
+import { useRedirect } from "../../hooks/useRedirect";
 
 const SignUpForm = () => {
+  useRedirect("loggedIn");
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
@@ -129,7 +131,7 @@ const SignUpForm = () => {
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://res.cloudinary.com/dannj5p63/image/upload/v1690295704/infoimage_g0uvdo.jpg"}
+          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
         />
       </Col>
     </Row>
